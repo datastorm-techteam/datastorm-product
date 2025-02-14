62c2bdadb8c23a0fff17d5b628c1809246fba7d8
Juhun Park <riann3207@gmail.com>
2025-02-14 13:39:06 -0500

    update index.html title and favicon; add About component and update navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
+// import logo from ".../insightlegi_logo.jpg";
+
 import Main from "./components/Main";
+import About from "./components/About";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,17 +13,24 @@ function App() {
   return (
     <div class="h-screen">
       <Router>
-        <nav className="flex justify-center items-center py-5">
-          <Link className="mx-10 text-black" to="/">
-            Home
-          </Link>
-          <Link className="mx-10 text-black" to="/about">
-            About
-          </Link>
+        <nav className="flex justify-between items-center py-5">
+          <div className="flex mx-10 text-black font-medium">
+            <img src="./insightlegi_logo.jpg" alt="logo" />
+            InsightLegi
+          </div>
+          <div>
+            <Link className="mx-10 text-black" to="/">
+              Home
+            </Link>
+            <Link className="mx-10 text-black" to="/about">
+              About
+            </Link>
+          </div>
         </nav>
         <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
           <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </Router>
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,12 @@
+import { React, useState } from "react";
+
+const About = () => {
+  return (
+    <div className="w-screen">
+      <div className="flex justify-center align-center">
+        <h1>About Page</h1>
+      </div>
+    </div>
+  );
+};
+export default About;
